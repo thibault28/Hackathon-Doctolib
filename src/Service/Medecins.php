64e912d756c51dff52bd1b69d05fcfcb99f9c3af
Admin <admin@admin.fr>
2020-06-26 10:54:38 +0200

    Search home and admin

--- src/Service/Medecins.php
+++ src/Service/Medecins.php
@@ -42,4 +42,22 @@ class Medecins
 
         return $json->records;
     }
+
+    public function getInfosMedecin(string $param)
+    {
+        $result = null;
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=annuaire-des-professionnels-de-sante&q='.$param);
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        $json = json_decode($content);
+
+        
+
+        return $json->records[0]->fields;
+    }
 }
